73198adbd0bfd83b18729bcd696aacf36fa77c5a
Antonin-Latour <106690380+Antonin-Latour@users.noreply.github.com>
2022-06-29 18:44:26 +0200

    Apport du bouton pour verrouiller totalement le système

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+input.onPinPressed(TouchPin.P0, function () {
+    able_tounlock = 0
+})
 input.onButtonPressed(Button.A, function () {
     if (input.logoIsPressed()) {
         servos.P0.setAngle(90)
@@ -50,9 +53,12 @@ input.onButtonPressed(Button.B, function () {
             `)
     }
 })
+let unlock_checknumber = 0
+let able_tounlock = 0
+able_tounlock = 0
 basic.showLeds(`
     . . . . .
-    . . . . .
+    . . # . .
     . . # . .
     . . . . .
     . . . . .
@@ -67,15 +73,44 @@ basic.showLeds(`
     # # # # #
     `)
 basic.forever(function () {
-    if (sonar.ping(
-    DigitalPin.P2,
-    DigitalPin.P2,
-    PingUnit.Centimeters
-    ) == 2) {
+    if (unlock_checknumber == 80) {
+        basic.showIcon(IconNames.Yes)
         servos.P0.setAngle(90)
         basic.clearScreen()
+        basic.showLeds(`
+            . # # . .
+            . . . # .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
+basic.forever(function () {
+    if (unlock_checknumber == 82) {
         basic.showIcon(IconNames.Yes)
-        basic.pause(2000)
+        servos.P0.setAngle(90)
+        basic.clearScreen()
+        basic.showLeds(`
+            . # # . .
+            . . . # .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
+basic.forever(function () {
+    if (able_tounlock == 1) {
+        basic.showString("" + (pins.analogReadPin(AnalogPin.P1)))
+        unlock_checknumber = pins.analogReadPin(AnalogPin.P1)
+    }
+})
+basic.forever(function () {
+    if (unlock_checknumber == 83) {
+        basic.showIcon(IconNames.Yes)
+        servos.P0.setAngle(90)
+        basic.clearScreen()
         basic.showLeds(`
             . # # . .
             . . . # .
@@ -90,13 +125,126 @@ basic.forever(function () {
     DigitalPin.P2,
     DigitalPin.P2,
     PingUnit.Centimeters
-    ) < 2) {
-        basic.showIcon(IconNames.No)
-        servos.P0.setAngle(0)
+    ) == 2) {
+        able_tounlock = 1
+    }
+})
+basic.forever(function () {
+    if (unlock_checknumber == 84) {
+        basic.showIcon(IconNames.Yes)
+        servos.P0.setAngle(90)
         basic.clearScreen()
         basic.showLeds(`
-            . . # . .
-            . # . # .
+            . # # . .
+            . . . # .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
+basic.forever(function () {
+    if (unlock_checknumber == 85) {
+        basic.showIcon(IconNames.Yes)
+        servos.P0.setAngle(90)
+        basic.clearScreen()
+        basic.showLeds(`
+            . # # . .
+            . . . # .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
+basic.forever(function () {
+    if (unlock_checknumber == 86) {
+        basic.showIcon(IconNames.Yes)
+        servos.P0.setAngle(90)
+        basic.clearScreen()
+        basic.showLeds(`
+            . # # . .
+            . . . # .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
+basic.forever(function () {
+    if (unlock_checknumber == 81) {
+        basic.showIcon(IconNames.Yes)
+        servos.P0.setAngle(90)
+        basic.clearScreen()
+        basic.showLeds(`
+            . # # . .
+            . . . # .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
+basic.forever(function () {
+    if (unlock_checknumber < 80) {
+        servos.P0.setAngle(0)
+    }
+})
+basic.forever(function () {
+    if (unlock_checknumber == 87) {
+        basic.showIcon(IconNames.Yes)
+        servos.P0.setAngle(90)
+        basic.clearScreen()
+        basic.showLeds(`
+            . # # . .
+            . . . # .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
+basic.forever(function () {
+    if (unlock_checknumber > 90) {
+        servos.P0.setAngle(0)
+    }
+})
+basic.forever(function () {
+    if (unlock_checknumber == 88) {
+        basic.showIcon(IconNames.Yes)
+        servos.P0.setAngle(90)
+        basic.clearScreen()
+        basic.showLeds(`
+            . # # . .
+            . . . # .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
+basic.forever(function () {
+    if (unlock_checknumber == 89) {
+        basic.showIcon(IconNames.Yes)
+        servos.P0.setAngle(90)
+        basic.clearScreen()
+        basic.showLeds(`
+            . # # . .
+            . . . # .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
+basic.forever(function () {
+    if (unlock_checknumber == 90) {
+        basic.showIcon(IconNames.Yes)
+        servos.P0.setAngle(90)
+        basic.clearScreen()
+        basic.showLeds(`
+            . # # . .
+            . . . # .
             # # # # #
             # # # # #
             # # # # #
